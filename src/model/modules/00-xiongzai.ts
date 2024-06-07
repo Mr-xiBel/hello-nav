7258f94f6f0f1c7a6b24c75f58ce8118f1227227
Mr-xiBel <35763527+Mr-xiBel@users.noreply.github.com>
2024-06-07 09:59:19 +0800

    Create 00-xiongzai.ts

---
+++ src/model/modules/00-xiongzai.ts
@@ -0,0 +1,17 @@
+export default <AppItem[]>[
+  {
+    homepage: 'https://ziyuanxiong.cn/',
+    repository: 'https://ziyuanxiong.cn/',
+    icon: 'logox.png',
+    keywords: ['vue'],
+    name: '资源熊',
+    lessRadius: true,
+  },
+  {
+    homepage: 'https://xibel.cn/',
+    repository: 'https://xibel/',
+    icon: 'logo1.png',
+    keywords: ['react'],
+    name: '西贝导航',
+  },
+]
